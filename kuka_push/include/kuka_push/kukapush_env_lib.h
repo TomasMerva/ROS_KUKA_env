4fc8b32ac46f5ea57ab6efd5ec3347eeafdf5bd8
TomasMerva <tmerva@rocketmail.com>
2020-08-08 14:33:56 +0200

    new comments

--- kuka_push/include/kuka_push/kukapush_env_lib.h
+++ kuka_push/include/kuka_push/kukapush_env_lib.h
@@ -1,57 +1,48 @@
-/**
- * \file kukapush_env_lib.h
- * \brief Header file for kukapush_env class
- *
- * \author Tomas Merva
- * \date 07.08.2020
- */
 #ifndef KUKAPUSH_ENV_LIB_H
 #define KUKAPUSH_ENV_LIB_H
 
 #include <kuka_push/include.h>
 #include <kuka_push/marker.h>
 
-/*! \class kukapush_env kukapush_env.h "include/kuka_push/kukapush_env.h"
-    \brief Class for creating KUKA_Push environment.
-*/
+//Class for creating KUKA_Push environment.
 class kukapush_env
 {
   public:
-    /** The constructor sets fixed orientation of the gripper, initializes MoveIt
-    *   and creates the trajectory publisher and the service client for reseting object in Gazebo
+    /* The constructor sets fixed orientation of the gripper, initializes MoveIt
+    *  and creates the trajectory publisher and the service client
+    *  for reseting object in Gazebo
     */
     kukapush_env(ros::NodeHandle *nh);
 
-    /** The method for receiving an action from the client and consequently after
-    *   40 ms returns the current state of the environment.
+    /* The method for receiving an action from the client
+    *  and sending a current state of the environment to the client
     */
     bool env_step(kuka_push::rl_env::Request &req,
                   kuka_push::rl_env::Response &resp);
 
-    /** The method for resetting the environment
+    /* The method for resetting the environment and sending
+    *  a current state of the environment thereafter to the client
     */
     void env_reset(kuka_push::rl_env::Response &resp);
-    
-    /** The callback method for getting the pose and velocity of the object.
-    */
+
+    // The callback method for getting the pose and velocity of the object.
     void objectCallback(const kuka_push::object_pose_vel::ConstPtr &msg);
 
   private:
-    void ExecuteAction(geometry_msgs::Pose &action_pose);
-    void RewardFunction();
-    void GetObservation();
-    void SetObservation(kuka_push::rl_env::Response &resp);
+    void ExecuteAction(geometry_msgs::Pose &action_pose); //Execute an action
+    void RewardFunction(); // Binary reward function
+    void GetObservation(); // Read and compute poses and velocities
+    void SetObservation(kuka_push::rl_env::Response &resp); // prepare a state for sending to client
 
     //class for generating goal
     RvizMarker marker;
 
-    //Trajectory publisher for controller
+    //Trajectory publisher for JointTrajectoryController
     ros::Publisher pub_joint_publisher;
 
-
     //Gazebo
-    ros::ServiceClient gazebo_client;
-    kuka_push::object_reset object_service;
+    ros::ServiceClient gazebo_client; // Gazebo client for generating new initial position of the object
+    kuka_push::object_reset object_service; // service message for reseting pose of the object
 
     //Moveit
     moveit::planning_interface::MoveGroupInterface group;
@@ -67,24 +58,26 @@ class kukapush_env
     trajectory_msgs::JointTrajectoryPoint desired_configuration;
     std::vector<double> goal_joint_values;
 
-    //RL State
-    geometry_msgs::PoseStamped gripper_pose; //current pose of the robot
-    geometry_msgs::Pose action_pose; //desired pose of the robot
-    geometry_msgs::Pose object_pose; //current pose of the object
-    geometry_msgs::Point goal_position; //goal position
-    geometry_msgs::Point object_gripper; //object position relative to gripper
-    geometry_msgs::Point object_goal; //object position relative to goal
+    // State of the environment
+    geometry_msgs::PoseStamped gripper_pose; // current pose of the robot
+    geometry_msgs::Pose action_pose; // desired pose of the robot
+    geometry_msgs::Pose object_pose; // current pose of the object
+    geometry_msgs::Point goal_position; // goal position
+    geometry_msgs::Point object_gripper; // object position relative to gripper
+    geometry_msgs::Point object_goal; // object position relative to goal
 
-    geometry_msgs::Twist object_vel; //object linear and angular velocity
+    geometry_msgs::Twist object_vel; // object linear and angular velocity
 
-    geometry_msgs::Point gripper_prev;
-    geometry_msgs::Point gripper_speed;
+    geometry_msgs::Point gripper_prev; // previous gripper's position
+    geometry_msgs::Point gripper_speed; // linear velocity of the gripper
 
-    double tolerance = 0.01;
-    bool found_approach_ik;
-    double time_move = 0.5;
+    double tolerance = 0.01; // tolerance for computing RewardFunction
+    bool found_approach_ik;  // flag for successfully finding IK solution
+    double time_move = 0.5;  // desired trajectory time between two points
 
-    //transforming agent's actions
+    /* variables needed for transforming agent's actions <-1.0, 1.0>
+    * into physical gripper's coordinates
+    */
     double delta_x = 0.19;
     double delta_y = 0.34;
     double delta_z = 0.15;
@@ -92,7 +85,7 @@ class kukapush_env
     double half_range_y = -0.34;
     double half_range_z = 0.24;
 
-    //RL variables
+    // reinforcement learning variables
     float reward = -1.0;
     bool done = false;
 };
--- kuka_push/src/kukapush_env_lib.cpp
+++ kuka_push/src/kukapush_env_lib.cpp
@@ -1,12 +1,13 @@
 #include <kuka_push/kukapush_env_lib.h>
 #include <chrono>
 
+// Random goal position
 std::random_device rd;
 std::mt19937 mt(rd());
-std::uniform_int_distribution<int> x_cord(600, 980);
-std::uniform_int_distribution<int> y_cord(-340, 340);
+std::uniform_int_distribution<int> x_cord(600, 980); //range of random x_coordinate
+std::uniform_int_distribution<int> y_cord(-340, 340); // range of random y_coordinate
 
-
+// Constructor
 kukapush_env::kukapush_env(ros::NodeHandle *nh)
   : marker(nh),
     group("manipulator"),
@@ -14,10 +15,10 @@ kukapush_env::kukapush_env(ros::NodeHandle *nh)
     current_state_ptr(group.getCurrentState()),
     goal_state_ptr(group.getCurrentState())
 {
-  //trajectory publisher
+  //trajectory publisher for JointTrajectoryController
   pub_joint_publisher = nh->advertise<trajectory_msgs::JointTrajectory>("position_trajectory_controller/command",1);
 
-  //Gazebo client
+  //Gazebo client for reseting the object's pose and generating new goal
   gazebo_client = nh->serviceClient<kuka_push::object_reset>("object_server");
 
   //Moveit
@@ -32,19 +33,20 @@ kukapush_env::kukapush_env(ros::NodeHandle *nh)
   action_pose.orientation.y = 0.000587617;
   action_pose.orientation.z = 0.707367;
   action_pose.orientation.w = 0.000508517;
-
-
 }
+
 
 bool kukapush_env::env_step(kuka_push::rl_env::Request &req,
                             kuka_push::rl_env::Response &resp)
 {
+  // check if agent requires resetting the environment or interacting with it
   if(req.reset)
   {
-    env_reset(resp);
+    env_reset(resp); //reset the environment
   }
-  else
+  else // interact with the environment
   {
+    // transforming desired agent's actions into physical coordinates
     action_pose.position.x = (static_cast<double>(req.action[0])*delta_x + delta_x) + half_range_x;
     action_pose.position.y = (static_cast<double>(req.action[1])*delta_y + delta_y) + half_range_y;
     action_pose.position.z = (static_cast<double>(req.action[1])*delta_z + delta_z) + half_range_z;
@@ -52,15 +54,16 @@ bool kukapush_env::env_step(kuka_push::rl_env::Request &req,
     {
       action_pose.position.z = 0.43;
     }
-    ExecuteAction(action_pose);
-    ros::Duration(0.03).sleep();
-    GetObservation();
-    RewardFunction();
-    SetObservation(resp);
+    ExecuteAction(action_pose); // execute client's action
+    ros::Duration(0.03).sleep(); // wait 30ms for action having an effect
+    GetObservation(); // get and compute state
+    RewardFunction(); // compute reward
+    SetObservation(resp); // prepare state for sending
   }
   return true;
 }
 
+// ------------------------------------------------
 void kukapush_env::env_reset(kuka_push::rl_env::Response &resp)
 {
   //generate new goal
@@ -82,11 +85,12 @@ void kukapush_env::env_reset(kuka_push::rl_env::Response &resp)
   gripper_prev.y = gripper_pose.pose.position.y;
   gripper_prev.z = gripper_pose.pose.position.z;
 
-  GetObservation();
-  RewardFunction();
-  SetObservation(resp);
+  GetObservation(); // get and compute state
+  RewardFunction(); // compute reward
+  SetObservation(resp); // prepare state for sending
 }
 
+// ------------------------------------------------
 void kukapush_env::ExecuteAction(geometry_msgs::Pose &action_pose)
 {
   //Calculate joint values based on action_pose
@@ -105,8 +109,7 @@ void kukapush_env::ExecuteAction(geometry_msgs::Pose &action_pose)
       arm_command.joint_names[i] = joint_name.str();
     }
 
-    /* Setting message for controller
-     */
+    // Setting message for controller
     arm_command.header.stamp = ros::Time::now();
     arm_command.header.frame_id = "base_link";
     arm_command.points.resize(1);
@@ -115,28 +118,30 @@ void kukapush_env::ExecuteAction(geometry_msgs::Pose &action_pose)
 
     // Publish trajectory into controller
     pub_joint_publisher.publish(arm_command);
-  }//if found_approach_ik
+  }
 
   else
   {
     ROS_WARN("No IK found");
     std::cout << "actions are: \n" << action_pose.position << std::endl;
-  }//else found_approach_ik
+  }
 }
 
+// ------------------------------------------------
 void kukapush_env::RewardFunction()
 {
+  // Checking if the object is within tolerance at the goal position
   if(((abs(object_pose.position.x - goal_position.x)) <= tolerance) &&
      ((abs(object_pose.position.y - goal_position.y)) <= tolerance) &&
      ((abs(object_pose.position.z - goal_position.z)) <= tolerance))
   {
-    reward = 0.0;
-    done = true;
+    reward = 0.0; // positive learning signal
+    done = true; // flag that the goal is accomplished
   }
   else
   {
-    reward = -1.0;
-    done = false;
+    reward = -1.0; //negative learning signal
+    done = false; // flag that the goal is NOT accomplished
   }
 }
 
@@ -165,7 +170,7 @@ void kukapush_env::GetObservation()
 
 void kukapush_env::SetObservation(kuka_push::rl_env::Response &resp)
 {
-  //assign gripper pose
+  //assign gripper's pose
   resp.state[0] = gripper_pose.pose.position.x;
   resp.state[1] = gripper_pose.pose.position.y;
   resp.state[2] = gripper_pose.pose.position.z;
@@ -174,7 +179,7 @@ void kukapush_env::SetObservation(kuka_push::rl_env::Response &resp)
   resp.state[5] = gripper_pose.pose.orientation.z;
   resp.state[6] = gripper_pose.pose.orientation.w;
 
-  //assign object pose
+  //assign object's pose
   resp.state[7] = object_pose.position.x;
   resp.state[8] = object_pose.position.y;
   resp.state[9] = object_pose.position.z;
@@ -183,19 +188,22 @@ void kukapush_env::SetObservation(kuka_push::rl_env::Response &resp)
   resp.state[12] = object_pose.orientation.z;
   resp.state[13] = object_pose.orientation.w;
 
-  //assign object position relative to gripper
+  //assign object's position relative to gripper's position
   resp.state[14] = object_gripper.x;
   resp.state[15] = object_gripper.y;
   resp.state[16] = object_gripper.z;
 
+  // assign linear velocities of the object
   resp.state[17] = object_vel.linear.x;
   resp.state[18] = object_vel.linear.y;
   resp.state[19] = object_vel.linear.z;
 
+  // assign angular velocities of the object
   resp.state[20] = object_vel.angular.x;
   resp.state[21] = object_vel.angular.y;
   resp.state[22] = object_vel.angular.z;
 
+  // assign linear velocities of the gripper
   resp.state[23] = gripper_speed.x;
   resp.state[24] = gripper_speed.y;
   resp.state[25] = gripper_speed.z;
@@ -216,7 +224,8 @@ void kukapush_env::SetObservation(kuka_push::rl_env::Response &resp)
 
 void kukapush_env::objectCallback(const kuka_push::object_pose_vel::ConstPtr &msg)
 {
-    object_pose.position = msg->pose.position; //  object_pose.position = msg->position;
+    // subscribe object's pose and velocities from Gazebo object's plugins
+    object_pose.position = msg->pose.position;
     object_pose.orientation = msg->pose.orientation;
 
     object_vel.linear = msg->twist.linear;
